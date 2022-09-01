c3ccfe103b0aeb57ec81e6807ef2236d173ebd47
Alan <alanssantos363@gmail.com>
2022-08-31 23:00:40 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Projeto realizando compras de pizzas.
---
+++ pizzas.js
@@ -0,0 +1,5 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'assets/img/pizza.png', price:20.90, sizes:['300g', '500g', '800g'], description:'pizza de Mussarela é a melhor que tem'},
+    {id:2, name:'Chocolate', img:'assets/img/pizza2.png', price:20.00, sizes:['300g', '500g', '800g'], description:'pizza de Chocolate é a melhor que tem'},
+    {id:3, name:'Queijo', img:'assets/img/pizza3.png', price:19.77, sizes:['300g', '500g', '800g'], description:'pizza de Queijo é a melhor que tem'}
+];
